c0d3873ec723defc57bb7f5f5f5b3513dd0d336a
Rinzler8806 <shane.rogers916@gmail.com>
2021-05-13 18:33:47 -0700

    updated About me - forgot to hit save

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ const About = () => {
                     <h1 className="about-heading">About Me</h1>
                     <p>
                     Hello! My name is Shane and I am a web developer from Sacramento, California, with a Bachelor of Business Administration degree in Business Management from the University of Hawaii. I began my post university life in finance, but eventually grew tired
-                    of the cubicle. I went back to school for construction management and became a project engineer for one of the world's largest construction companies, where I played a role in transforming the skyline of the San Francisco Bay area. 
+                    of the cubicle. I went back to school for Construction Management and became a project engineer for one of the world's largest construction companies, where I played a role in transforming the skyline of the San Francisco Bay area. 
                     <br></br>
                     <br></br>
                     While construction can be a rewarding profession, I have found myself drawn to creating in the digital space and have chosen to make a career change.
